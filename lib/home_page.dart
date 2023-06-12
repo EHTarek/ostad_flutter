4de8764df63ebd2c7c2c3af1ba1647fd98fdaac5
EHTarek <tariqul.191002037@green.ac.bd>
2023-06-12 18:25:43 +0600

    module 6 assignment

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,6 +6,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<String> myImageList = [
+    'https://media.istockphoto.com/id/931479532/photo/mountain-valley-during-sunrise-beutiful-natural-landsscape-in-the-summer-time.jpg?s=612x612&w=0&k=20&c=ND9k1tgsUbvsjylYrniloA_C8ICA4GXFZP-8dgvzDxY=',
+    'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg',
+    'https://images.unsplash.com/reserve/Af0sF2OS5S5gatqrKzVP_Silhoutte.jpg?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+    'https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg',
+    'https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_1280.jpg',
+    'https://i0.wp.com/www.flutterbeads.com/wp-content/uploads/2022/01/add-image-in-flutter-hero.png?fit=2850%2C1801&ssl=1'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,58 +45,28 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             Wrap(
-              spacing: 8,
-              runSpacing: 8,
+              spacing: 5,
+              runSpacing: 5,
               children: [
-                MyPhotoGrid(
-                    imgLink:
-                        'https://media.istockphoto.com/id/931479532/photo/mountain-valley-during-sunrise-beutiful-natural-landsscape-in-the-summer-time.jpg?s=612x612&w=0&k=20&c=ND9k1tgsUbvsjylYrniloA_C8ICA4GXFZP-8dgvzDxY='),
-                MyPhotoGrid(
-                    imgLink:
-                        'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg'),
-                MyPhotoGrid(
-                    imgLink:
-                        'https://images.unsplash.com/reserve/Af0sF2OS5S5gatqrKzVP_Silhoutte.jpg?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80'),
-                MyPhotoGrid(
-                    imgLink:
-                        'https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg'),
-                MyPhotoGrid(
-                    imgLink:
-                        'https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_1280.jpg'),
-                MyPhotoGrid(
-                    imgLink:
-                        'https://i0.wp.com/www.flutterbeads.com/wp-content/uploads/2022/01/add-image-in-flutter-hero.png?fit=2850%2C1801&ssl=1'),
+                for (int x = 0; x < myImageList.length; x++) myImageGrid(x)
               ],
             ),
-            ListTile(
-              title: const Text('Sample Photo 1'),
-              subtitle: const Text('Category 1'),
-              leading: Image.network(
-                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg',
-                fit: BoxFit.cover,
-                width: 40,
-                height: 40,
-              ),
-            ),
-            ListTile(
-              title: const Text('Sample Photo 2'),
-              subtitle: const Text('Category 2'),
-              leading: Image.network(
-                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg',
-                fit: BoxFit.cover,
-                width: 40,
-                height: 40,
-              ),
-            ),
-            ListTile(
-              title: const Text('Sample Photo 3'),
-              subtitle: const Text('Category 3'),
-              leading: Image.network(
-                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg',
-                fit: BoxFit.cover,
-                width: 40,
-                height: 40,
-              ),
+            ListView(
+              primary: false,
+              shrinkWrap: true,
+              children: [
+                for (int x = 0; x < myImageList.length; x++)
+                  ListTile(
+                    title: Text('Sample Photo ${x + 1}'),
+                    subtitle: Text('Category ${x + 1}'),
+                    leading: Image.network(
+                      myImageList[x].toString(),
+                      fit: BoxFit.cover,
+                      width: 40,
+                      height: 40,
+                    ),
+                  ),
+              ],
             ),
           ],
         ),
@@ -104,20 +83,9 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-}
 
-class MyPhotoGrid extends StatefulWidget {
-  String imgLink;
-
-  MyPhotoGrid({required this.imgLink});
-
-  @override
-  State<MyPhotoGrid> createState() => _MyPhotoGridState();
-}
-
-class _MyPhotoGridState extends State<MyPhotoGrid> {
-  @override
-  Widget build(BuildContext context) {
+  ElevatedButton myImageGrid(int index) {
+    double size = 100;
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
         padding: EdgeInsets.zero,
@@ -125,39 +93,31 @@ class _MyPhotoGridState extends State<MyPhotoGrid> {
       onPressed: () {
         setState(() {
           ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Text('Clicked on photo!'),
-            ),
+            const SnackBar(content: Text('Clicked on photo!')),
           );
         });
       },
-      child: Container(
-        height: 100,
-        width: 100,
-        child: Stack(
-          children: [
-            Image.network(
-              width: 100,
-              height: 100,
-              widget.imgLink,
-              fit: BoxFit.cover,
-            ),
-            Positioned(
-              bottom: 0,
-              left: 15,
+      child: Stack(
+        children: [
+          Container(
+            height: size,
+            width: size,
+            child:
+                Image.network(myImageList[index].toString(), fit: BoxFit.cover),
+          ),
+          Positioned(
+            width: size,
+            height: size,
+            child: Align(
+              alignment: Alignment.bottomCenter,
               child: Container(
+                padding: EdgeInsets.all(8),
                 color: Colors.black.withOpacity(0.5),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                child: const Text(
-                  'Caption',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.white),
-                ),
+                child: Text('Caption'),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
