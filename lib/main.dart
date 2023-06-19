90c21cd36c51dac822fc5f387edc9e6fd3a93158
EHTarek <tariqul.191002037@green.ac.bd>
2023-06-19 23:51:43 +0600

    module 7 live test

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'My Shopping List',
+      title: 'Counter App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
